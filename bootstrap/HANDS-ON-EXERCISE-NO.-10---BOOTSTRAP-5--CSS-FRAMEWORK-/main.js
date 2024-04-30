933aa7318dceab307ecaaf1d5aa85fb4767b082a
Mebalog <156916171+Mebalog@users.noreply.github.com>
2024-05-01 00:04:19 +0800

    Delete bootstrap/HANDS-ON-EXERCISE-NO.-10---BOOTSTRAP-5--CSS-FRAMEWORK- directory

--- bootstrap/HANDS-ON-EXERCISE-NO.-10---BOOTSTRAP-5--CSS-FRAMEWORK-/main.js
+++
@@ -1,9 +0,0 @@
-function myFunction() {
-    var x = document.getElementById("inpForStudPassword");
-
-    if (x.type === "password") {
-        x.type = "text";
-    } else {
-        x.type = "password";
-    }
-}
